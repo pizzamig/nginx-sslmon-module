9a963fd77e57f3ec7b7d9fbf292c75c1a58718cb
Luca Pizzamiglio <luca.pizzamiglio@trivago.com>
2016-11-14 12:11:38 +0100

    Several typo fixes

--- ngx_http_sslmon.c
+++ ngx_http_sslmon.c
@@ -5,7 +5,7 @@
 #include <ngx_http.h>
 #include <nginx.h>
 
-static nvx_int_t
+static ngx_int_t
 ngx_http_sslmon_init( ngx_conf_t *cf );
 
 /*
@@ -39,9 +39,9 @@ ngx_http_sslmon_init( ngx_conf_t *cf )
 	ngx_http_core_main_conf_t *cmcf;
 	ngx_http_handler_pt *h;
 
-	cmcf = ngx_http_conf_get_module_main_conf( cf, ngx_http_core_modue );
+	cmcf = ngx_http_conf_get_module_main_conf( cf, ngx_http_core_module );
 
-	h = ngx_arra_ypush(&cmcf->phases[NGX_HTTP_LOG_PHASE].handlers);
+	h = ngx_array_push(&cmcf->phases[NGX_HTTP_LOG_PHASE].handlers);
 	if ( h == NULL ) {
 		return NGX_ERROR;
 	}
