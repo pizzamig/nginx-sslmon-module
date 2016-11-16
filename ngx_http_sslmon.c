229023149bd193f044b446ca853a0b41ef758c0c
Luca Pizzamiglio <luca.pizzamiglio@trivago.com>
2016-11-16 15:19:42 +0100

    Adding statistic structur and cleaning up
    Moreover, added a parameter to determine when a request has to be
    considered slow

--- ngx_http_sslmon.c
+++ ngx_http_sslmon.c
@@ -5,6 +5,9 @@
 #include <ngx_http.h>
 #include <nginx.h>
 
+/* ****************************** */
+/* Function prototype definitions */
+/* ****************************** */
 static ngx_int_t
 ngx_http_sslmon_init( ngx_conf_t *cf );
 
@@ -20,27 +23,58 @@ ngx_http_sslmon_init_process( ngx_cycle_t * cx );
 static ngx_int_t
 ngx_http_sslmon_exit_process( ngx_cycle_t * cx );
 
-typedef struct {
-	ngx_str_t filename;
-	int fd;
-	unsigned long counter;
-} ngx_http_sslmon_main_conf_t;
+/* ************************ */
+/* Types/structs definition */
+/* ************************ */
+
+typedef double ngx_avg;		/* avarage */
+typedef long unsigned ngx_cnt;	/* counter */
 
 typedef struct {
-	unsigned long counter;
+	ngx_cnt counter;
+/* request time statistic */
+	ngx_avg rqt_time;	/* avarage request time */ 
+	ngx_avg up_time;	/* avarage upstream time */
+	ngx_avg net_rqt_time;	/* avarage net request time */
+				/* net request time is the time */
+				/* spent in nginx only */
+	ngx_cnt slow_requests;	/* requests slower than 200ms */
+	ngx_cnt reused_session;	/* reused sessions */
 } ngx_http_sslmon_stats_t;
 
+typedef struct {
+	ngx_uint_t	update_period;
+	ngx_uint_t	slow_request_time;
+	int fd;
+	ngx_http_sslmon_stats_t * stats;
+} ngx_http_sslmon_main_conf_t;
+
+/* ************************ */
+/* Nginx module definitions */
+/* ************************ */
+
+/* How often the statistic file has to be written */
+/* default: every 1000 requests */
+#define SSLMON_DEFAULT_UPDATE_PERIOD 1000
+
+/* A reuqest is considered slow if it takes more than 200ms */
+#define SSLMON_DEFAULT_SLOW_REQUEST_TIME 200
 static ngx_command_t ngx_http_sslmon_commands[] = {
 
-        { ngx_string("sslmon_output_file"),
+        { ngx_string("sslmon_update_period"),
           NGX_HTTP_MAIN_CONF | NGX_CONF_TAKE1,
-          ngx_conf_set_str_slot,
+          ngx_conf_set_num_slot,
           NGX_HTTP_LOC_CONF_OFFSET,
-	  offsetof(ngx_http_sslmon_main_conf_t, filename),
+	  offsetof(ngx_http_sslmon_main_conf_t, update_period),
 	  NULL },
 
+        { ngx_string("sslmon_slow_request_time"),
+          NGX_HTTP_MAIN_CONF | NGX_CONF_TAKE1,
+          ngx_conf_set_msec_slot,
+          NGX_HTTP_LOC_CONF_OFFSET,
+	  offsetof(ngx_http_sslmon_main_conf_t, slow_request_time),
+	  NULL },
 	ngx_null_command
-
 };
 
 static ngx_http_module_t  ngx_http_sslmon_module_ctx = {
@@ -85,23 +119,53 @@ ngx_http_sslmon_create_main_conf(ngx_conf_t *cf)
 		ngx_log_error(NGX_LOG_DEBUG, cf->log, 0,
 			"sslmon_create_main_conf: conf store at %p", conf);
 	}
+	conf->update_period = NGX_CONF_UNSET_UINT;
+	conf->slow_request_time = NGX_CONF_UNSET_UINT;
 	conf->fd = NGX_CONF_UNSET;
-	conf->counter = 0;
+
+	ngx_http_sslmon_stats_t *stats;
+	stats = ngx_pcalloc(cf->pool, sizeof(ngx_http_sslmon_stats_t));
+	if( stats == NULL ) {
+		ngx_log_error(NGX_LOG_ERR, cf->log, 0,
+			"sslmon_create_main_conf: not able to allocate stats");
+		return NULL;
+	} else {
+		ngx_log_error(NGX_LOG_DEBUG, cf->log, 0,
+			"sslmon_create_main_conf: stats store at %p", conf);
+	}
+	conf->stats = stats;
 	return conf;
 }
 
-static char *
-ngx_http_sslmon_merge_main_conf(ngx_conf_t *cf, void *conf)
+static void
+ngx_http_sslmon_reset_stats( ngx_http_sslmon_stats_t * s )
 {
-	/* 
+	/* resetting counters */
+	s->counter = s->slow_requests = s->reused_session = 0;
+
+	/* resetting avarages */
+	s->rqt_time = s->up_time = s->net_rqt_time = 0;
+}
+
+
+static char *
+ngx_http_sslmon_merge_main_conf(ngx_conf_t *cf, void *c)
+{
+	ngx_http_sslmon_main_conf_t * conf = c;
+	ngx_http_sslmon_stats_t * stats = conf->stats;
+	if( conf->update_period == NGX_CONF_UNSET_UINT ) {
+		conf->update_period = SSLMON_DEFAULT_UPDATE_PERIOD;
+	}
+	if( conf->slow_request_time == NGX_CONF_UNSET_UINT ) {
+		conf->slow_request_time = SSLMON_DEFAULT_SLOW_REQUEST_TIME;
+	}
 	ngx_log_error(NGX_LOG_NOTICE, cf->log, 0,
-		"sslmon_merge_main_conf: starting");
-	*/
+		"sslmon_create_main_conf: update_period %u, slow_request %u ms", 
+		conf->update_period, conf->slow_request_time);
 	return NGX_CONF_OK;
 }
 
 #define SSLMON_FILENAME_BASE "/var/log/nginx/sslmon/sslmon"
-#define SSLMON_STATISTIC_FILENAME "/var/log/nginx/sslmon/sslmon.log"
 #define SSLMON_FILENAME_MAXSIZE 512
 static ngx_int_t
 ngx_http_sslmon_init_process( ngx_cycle_t * cx )
@@ -129,9 +193,7 @@ ngx_http_sslmon_init_process( ngx_cycle_t * cx )
 	conf->fd = ngx_open_file( filename.data, NGX_FILE_WRONLY,
 		NGX_FILE_TRUNCATE | NGX_FILE_NONBLOCK,
 		S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH );
-	/*
-	conf->fd = open( SSLMON_STATISTIC_FILENAME, O_WRONLY | O_CREAT | O_TRUNC );
-	*/
+
 	if ( conf->fd == -1 ) {
 		ngx_log_error(NGX_LOG_ERR, cx->log, 0,
 			"sslmon_init_process: file not open (%d)", errno);
@@ -146,7 +208,9 @@ static ngx_int_t
 ngx_http_sslmon_handler( ngx_http_request_t *r )
 {
 	ngx_http_sslmon_main_conf_t * conf;
+	ngx_http_sslmon_stats_t * stats;
 	conf = ngx_http_get_module_main_conf( r, ngx_http_sslmon_module );
+	stats = conf->stats;
 
 	ngx_str_t var_name = ngx_string("ssl_cipher");
 	ngx_uint_t key;
@@ -166,14 +230,14 @@ ngx_http_sslmon_handler( ngx_http_request_t *r )
 				 var_name.data, vv->data);
 		}
 	}
-	conf->counter++;
+	stats->counter++;
 	if( conf->fd != NGX_CONF_UNSET ) {
 		off_t seek_rc;
 		seek_rc = lseek( conf->fd, 0, SEEK_SET );
-		dprintf( conf->fd, "%lu\n", conf->counter );
+		dprintf( conf->fd, "%lu\n", stats->counter );
 		ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0,
 			"sslmon_handler: Updating counter %d from pid %d",
-			conf->counter, ngx_getpid());
+			stats->counter, ngx_getpid());
 	} else {
 		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
 			"sslmon_handler: fd not set - conf %p", conf);
