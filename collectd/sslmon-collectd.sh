b05cc64ff3e4f23533278063b95b0443db8c7f4c
Luca Pizzamiglio <luca.pizzamiglio@trivago.com>
2016-11-18 13:38:34 +0100

    Fixing sslmon-collectd to really work
    NOTICE: the path to reach merger.awk needs a better solution the hardcoded

--- collectd/sslmon-collectd.sh
+++ collectd/sslmon-collectd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 UPDATE_PERIOD=15
+IDENTIFIER="$(hostname)"/ssl/
 while true;
 do
 	start_epoch=$(date +%s)
-	export TMPDIR=$PWD
 	if [ -z "$(ls /var/log/nginx/sslmon/sslmon-* 2>/dev/null)" ]; then
 		data_epoch=${start_epoch}
 		requests=0
@@ -14,7 +14,7 @@ do
 		ssl_time=0
 	else
 		MERGEFILE=$(mktemp -t sslmon-XXX)
-		./merger.awk /var/log/nginx/sslmon/* > ${MERGEFILE}
+		/home/lpizzamiglio/work/nginx-ssl-monitor/nginx-sslmon-module/collectd/merger.awk /var/log/nginx/sslmon/* > ${MERGEFILE}
 		data_epoch=$(awk -F= '/^epoc/ { print $2 }' ${MERGEFILE})
 		if [ ${data_epoch} -eq 0 ]; then
 			data_epoch=${start_epoch}
@@ -29,7 +29,12 @@ do
 		rm -rf ${MERGEFILE}
 	fi
 	#rrdtool updatev sslmon.rrd -s ${data_epoch}:${requests}:${slow_requests}:${reused_sessions}:${request_time}:${upstream_time}:${ssl_time}
-	echo PUTVAL "" interval=$UPDATE_PERIOD ${data_epoch}:${requests}:${slow_requests}:${reused_sessions}:${request_time}:${upstream_time}:${ssl_time}
+	echo PUTVAL "${IDENTIFIER}counter-requests" interval=$UPDATE_PERIOD ${data_epoch}:${requests}
+	echo PUTVAL "${IDENTIFIER}counter-slow_requests" interval=$UPDATE_PERIOD ${data_epoch}:${slow_requests}
+	echo PUTVAL "${IDENTIFIER}counter-reused_sessions" interval=$UPDATE_PERIOD ${data_epoch}:${reused_sessions}
+	echo PUTVAL "${IDENTIFIER}total_time_in_ms-request_time" interval=$UPDATE_PERIOD ${data_epoch}:${request_time}
+	echo PUTVAL "${IDENTIFIER}total_time_in_ms-upstream_time" interval=$UPDATE_PERIOD ${data_epoch}:${upstream_time}
+	echo PUTVAL "${IDENTIFIER}total_time_in_ms-ssl_time" interval=$UPDATE_PERIOD ${data_epoch}:${ssl_time}
 	now=$(date +%s)
 	if [ $(( now - start_epoch )) -le $UPDATE_PERIOD ]; then
 		sleep $(( $UPDATE_PERIOD + start_epoch - now ))
