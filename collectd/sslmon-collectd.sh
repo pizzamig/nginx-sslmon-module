66518bd7ad322b50966e647bd0e569acd1ce3947
Luca Pizzamiglio <luca.pizzamiglio@trivago.com>
2016-11-20 19:50:22 +0100

    Including the awk script in the shell script, to avoid possible absolute paths problem
    The shell script can run from an unknownw position, so we should provide an absolute
    pathname to invoke merger.awk.
    It's doable, but integrating the solution in one file, makes the deploy easier,
    without creating too mucn confusion in the original file

--- collectd/sslmon-collectd.sh
+++ collectd/sslmon-collectd.sh
@@ -1,6 +1,46 @@
 #!/bin/sh
 UPDATE_PERIOD=15
 IDENTIFIER="$(hostname)"/ssl/
+
+merger()
+{
+	awk '
+	BEGIN { epoch_counter=0;
+		epoch=0;
+		counter=0;
+		slow_requests=0;
+		reused_sessions=0;
+		avg_rt=0.0;
+		avg_ut=0.0;
+		avg_net_rt=0.0;
+		FS="=";
+	}
+	/^epoch/	{ if ( epoch < $2 )
+				epoch = $2;
+			  epoch_counter++;
+			}
+	/^counter/		{ counter = counter + $2 }
+	/^slow_requests/	{ slow_requests = slow_requests + $2 }
+	/^reused_sessions/	{ reused_sessions = reused_sessions + $2 }
+	/^avg_rt/		{ avg_rt = avg_rt + $2 }
+	/^avg_ut/		{ avg_ut = avg_ut + $2 }
+	/^avg_net_rt/		{ avg_net_rt = avg_net_rt + $2 }
+	END {
+		if ( epoch_counter == 0 )
+			epoch_counter = 1;
+		avg_rt = avg_rt / epoch_counter;
+		avg_ut = avg_ut / epoch_counter;
+		avg_net_rt = avg_net_rt / epoch_counter;
+		printf "epoch=%d\n",epoch;
+		printf "counter=%d\n",counter;
+		printf "slow_requests=%d\n", slow_requests;
+		printf "reused_sessions=%d\n", reused_sessions;
+		printf "avg_rt=%d\n", avg_rt;
+		printf "avg_ut=%d\n", avg_ut;
+		printf "avg_net_rt=%d\n", avg_net_rt;
+	}' "$@"
+}
+
 while true;
 do
 	start_epoch=$(date +%s)
@@ -14,7 +54,7 @@ do
 		ssl_time=0
 	else
 		MERGEFILE=$(mktemp -t sslmon-XXX)
-		/home/lpizzamiglio/work/nginx-ssl-monitor/nginx-sslmon-module/collectd/merger.awk /var/log/nginx/sslmon/* > ${MERGEFILE}
+		merger /var/log/nginx/sslmon/* > ${MERGEFILE}
 		data_epoch=$(awk -F= '/^epoc/ { print $2 }' ${MERGEFILE})
 		if [ ${data_epoch} -eq 0 ]; then
 			data_epoch=${start_epoch}
